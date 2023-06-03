1e09d69263d10d63d5c6d2f2a045e94558ce1852
yitong86 <sophiajiang86@hotmail.com>
2023-06-03 16:18:44 -0400

    Initial commit

---
+++ src/com/sophia/blind75/ValidParentheses.java
@@ -0,0 +1,23 @@
+package com.sophia.blind75;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+   // class Solution {
+        public boolean isValid(String s) {
+            Stack<Character> stack = new Stack<Character>();
+            for(Character c : s.toCharArray()){
+                if(c == '('){
+                    stack.push(')');
+                }else if(c =='{'){
+                    stack.push('}');
+                }else if( c == '['){
+                    stack.push(']');
+                }else if(stack.isEmpty() || stack.pop() != c){
+                    return false;
+                }
+            }
+            return stack.isEmpty();
+        }
+   // }
+}
