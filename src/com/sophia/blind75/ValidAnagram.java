1a5f9c81b80b02dca99090bbd8f3e9b34d01ac39
yitong86 <sophiajiang86@hotmail.com>
2023-06-10 17:15:00 -0400

    addMETHOD

--- src/com/sophia/blind75/ValidAnagram.java
+++ src/com/sophia/blind75/ValidAnagram.java
@@ -1,5 +1,6 @@
 package com.sophia.blind75;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class ValidAnagram {
@@ -23,3 +24,17 @@ public class ValidAnagram {
                 return true;
             }
         }
+
+        //What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+// if(s.length() != t.length()) return false;
+//        Map<Character,Integer> map = new HashMap<Character, Integer>();
+//        for(int i = 0;i<s.length();i++){
+//            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0) +1);
+//            map.put(t.chaAt(i),map.getOrDefault(t.charAt(i),0) -1);
+//            for(char c: map.keySet()){
+//                if(map.get(c) != 0){
+//                    return false;
+//        }
+//        }
+//            return true;
+//        } }
