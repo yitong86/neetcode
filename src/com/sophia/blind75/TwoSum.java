159599ca488d83bcd3629bdddb0720380c83cdd8
yitong86 <sophiajiang86@hotmail.com>
2023-06-15 15:25:14 -0400

    addComments

--- src/com/sophia/blind75/TwoSum.java
+++ src/com/sophia/blind75/TwoSum.java
@@ -20,3 +20,10 @@ public class TwoSum {
       //  }
     }
 }
+//for (int i = 0;i<nums.length;i++){
+//    for(int j = 0;j<nums.length;j++){
+//        if(nums[i] + nums[j] == target){
+//            return new int[]{i,j}
+//        }
+//        }
+//        }
