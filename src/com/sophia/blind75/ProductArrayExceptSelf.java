44ef3e63ea95a2d4b958604df843f88ee36d76f9
yitong86 <sophiajiang86@hotmail.com>
2023-06-22 13:47:48 -0400

    update

--- src/com/sophia/blind75/ProductArrayExceptSelf.java
+++ src/com/sophia/blind75/ProductArrayExceptSelf.java
@@ -5,23 +5,30 @@ import java.util.*;
 public class ProductArrayExceptSelf {
     public int[] productexceptself(int[] nums) {
         //Input: nums = [1,2,3,4]
+        //[2*3*3,1*3*4,...]
+        //[24,12,8,6]
         //Output: [24,12,8,6]
-        int N = nums.length;
-        int[] left_products = new int[N];
-        int[] right_products = new int[N];
-        int[] output_arr = new int[N];
+        //array to store all left multiplication
+        int[] left_products = new int[nums.length];
+        ////array to store all right multiplication
+        int[] right_products = new int[nums.length];
 
         left_products[0] = 1;
-        right_products[N-1] = 1;
-        for(int i = 1;i<N;i++){
-            left_products[i] = nums[i-1]  * left_products[i-1];
+        //first do not have any element on left
+        for(int i = 1;i < nums.length;i++){
+            left_products[i] = left_products[i-1] * nums[i-1];
+            //left_products[2] = left_products[2-1] * nums[2-1]
+            //                  ==3 * 4 =12
 
         }
-        for(int i = N-2;i>=0 ;i--){
-            right_products[i] = nums[i+1] * right_products[i+1];
+        right_products[nums.length-1] = 1;
+        for(int i = nums.length-2;i>=0 ;i--){
+            right_products[i] = right_products[i+1] * nums[i+1] ;
 
         }
-        for(int i = 0;i<N;i++){
+
+        int[] output_arr = new int[nums.length];
+        for(int i = 0;i < nums.length;i++){
             output_arr[i] = left_products[i] * right_products[i];
 
         }
