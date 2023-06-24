19597d5b41bd0efa5b444e49f26f74403b4428bb
yitong86 <sophiajiang86@hotmail.com>
2023-06-24 14:37:19 -0400

    threeSum

--- src/com/sophia/blind75/ValidPalindrome.java
+++ src/com/sophia/blind75/ValidPalindrome.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 public class ValidPalindrome {
 
     public boolean isPalindrome(String s) {
+        //s= s.toLowerCase().replaceAll("[A-Za-z0-9]","")
         String res = "";
         //loop though character in a string
         for(Character c :s.toCharArray()){
