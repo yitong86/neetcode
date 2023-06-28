5e30db31449746903784fbe873bcb47c29c509b7
yitong86 <sophiajiang86@hotmail.com>
2023-06-28 14:59:16 -0400

    longestSubWithoutRepeat

--- src/com/sophia/blind75/LongestSubstringWithoutRepeating.java
+++ src/com/sophia/blind75/LongestSubstringWithoutRepeating.java
@@ -7,10 +7,13 @@ public class LongestSubstringWithoutRepeating {
     public int lengthOfLongestSubstring(String s) {
         int maxLength = 0;
         Map<Character, Integer> visitedCharacters = new HashMap<>();
+
+       //{a,0,b,1 } 0->position index
         for (int right = 0, left = 0; right < s.length(); right++) {
             char currentCharacter = s.charAt(right);
-            if (visitedCharacters.containsKey(currentCharacter) && visitedCharacters.get(currentCharacter) >= left) {
-
+            if (visitedCharacters.containsKey(currentCharacter) &&
+                    visitedCharacters.get(currentCharacter) >= left) {
+                    left = visitedCharacters.get(currentCharacter)+1;
             }
             maxLength = Math.max(maxLength, right - left + 1);
             visitedCharacters.put(currentCharacter, right);
