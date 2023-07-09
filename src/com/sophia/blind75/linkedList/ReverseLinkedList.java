004b64ab72bf10d3711d822a7085276c6dca51c2
yitong86 <sophiajiang86@hotmail.com>
2023-07-08 21:40:33 -0400

    linkedlistReverse

---
+++ src/com/sophia/blind75/linkedList/ReverseLinkedList.java
@@ -0,0 +1,23 @@
+package com.sophia.blind75.linkedList;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            while(head != null){
+                ListNode next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+            return prev;
+    }
+}
