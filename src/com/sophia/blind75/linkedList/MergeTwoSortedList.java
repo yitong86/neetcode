a8043f76820e496e6cf60064a3a112524fc8c577
yitong86 <sophiajiang86@hotmail.com>
2023-07-11 22:07:18 -0400

    update

--- src/com/sophia/blind75/linkedList/MergeTwoSortedList.java
+++ src/com/sophia/blind75/linkedList/MergeTwoSortedList.java
@@ -16,12 +16,13 @@ package com.sophia.blind75.linkedList;
 public class MergeTwoSortedList {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
+
         // Create a sentinal/dummy node to start
         ListNode returnNode = new ListNode(Integer.MIN_VALUE);
 
         // Create a copy of this node to iterate while solving the problem
         ListNode headNode = returnNode;
-
+//reverse the list
         // Traverse till one of the list reaches the end
         while (l1 != null && l2 != null) {
 
