4c5e7709f2e5ea19e2945c7a526e4e65c7f3805d
yitong86 <sophiajiang86@hotmail.com>
2023-07-14 21:04:20 -0400

    LinkedListcycle

---
+++ src/com/sophia/blind75/linkedList/LinkedListCycle.java
@@ -0,0 +1,29 @@
+package com.sophia.blind75.linkedList;
+
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
