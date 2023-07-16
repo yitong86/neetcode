f8f0e8c78d5d03f9e2291e7c6ea355ff5cfd1ae0
yitong86 <sophiajiang86@hotmail.com>
2023-07-15 22:10:30 -0400

    addComments

--- src/com/sophia/blind75/linkedList/LinkedListCycle.java
+++ src/com/sophia/blind75/linkedList/LinkedListCycle.java
@@ -15,11 +15,15 @@ package com.sophia.blind75.linkedList;
  */
 public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
+        //start a slow pointer and fast pointer
         ListNode fast = head;
         ListNode slow = head;
+
+        //advance both pointers
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
+            //if they meet, means we found a loop
             if (fast == slow) {
                 return true;
             }
