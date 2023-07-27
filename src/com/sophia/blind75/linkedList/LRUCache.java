38003248f86ca8018c9644120655bc38bd294aaa
yitong86 <sophiajiang86@hotmail.com>
2023-07-26 20:51:48 -0400

    LRUCache

---
+++ src/com/sophia/blind75/linkedList/LRUCache.java
@@ -0,0 +1,81 @@
+package com.sophia.blind75.linkedList;
+
+import org.w3c.dom.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+        final Node head = new Node();
+        final Node tail = new Node();
+
+        Map<Integer,Node> node_map;
+        int cache_capacity;
+    public LRUCache(int capacity) {
+        node_map = new HashMap<>();
+        this.cache_capacity = capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        int result = -1;
+        Node node = node_map.get(key);
+        if( node != null){
+            result = node.val;
+            remove(node);
+            add(node);
+        }
+        return result;
+    }
+
+    public void put(int key, int value) {
+        Node node = node_map.get(key);
+        if(node!= null){
+            remove(node);
+            node.val = value;
+
+            add(node);
+        }else{
+            if(node_map.size() == cache_capacity){
+                node_map.remove(tail.prev.key);
+                remove(tail.prev);
+
+            }
+            Node new_node = new Node();
+            new_node.key = key;
+            new_node.val = value;
+
+            node_map.put(key,new_node);
+            add(new_node);
+        }
+    }
+    public void add(Node node){
+        Node head_next = node.next;
+        node.next = head_next;
+        head_next.prev = node;
+        node.prev = head;
+    }
+    public void remove(Node node){
+        Node next_node = node.next;
+        Node prev_node = node.prev;
+
+        next_node.prev = prev_node;
+        prev_node.next = next_node;
+    }
+
+    public class Node{
+        int key;
+        int val;
+        Node prev;
+        Node next;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
