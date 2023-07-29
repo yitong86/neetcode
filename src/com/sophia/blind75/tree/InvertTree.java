050067e6aa7acf129103b67d49c88771bef17165
yitong86 <sophiajiang86@hotmail.com>
2023-07-29 17:14:00 -0400

    update

--- src/com/sophia/blind75/tree/InvertTree.java
+++ src/com/sophia/blind75/tree/InvertTree.java
@@ -20,47 +20,16 @@ import java.util.Queue;
  *     }
  * }
  */
-public class InvertTree {
-
-    TreeNode invertTreeRecursive(TreeNode root) {
-
-        if (root == null)
-            return null;
-
-        TreeNode temp = root.right;
-        root.right = root.left;
-        root.left = temp;
-
-        invertTree(root.left);
-        invertTree(root.right);
-
+class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        //swap
+        root.left = right;
+        root.right = left;
         return root;
     }
-
-    TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-
-        final Queue<TreeNode> queue = new LinkedList<>();
-       //start with the first root node
-        queue.add(root);
-//run a loop till this queue is not empty
-        while (!queue.isEmpty()) {
-            //remove leaste node
-            final TreeNode node = queue.poll();
-
-            // Swap nodes
-            final TreeNode temp = node.left;
-            node.left = node.right;
-            node.right = temp;
-
-            // Add left and right of this node to the queue
-            if (node.left != null) queue.add(node.left);
-
-            if (node.right != null) queue.add(node.right);
-        }
-        return root;
-    }
-
 }
