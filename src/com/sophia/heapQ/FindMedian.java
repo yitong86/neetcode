7aaecd57fed05cd1d4fa12b71bacabf56e65c15c
yitong86 <sophiajiang86@hotmail.com>
2023-09-30 14:17:01 -0400

    FindMedian

---
+++ src/com/sophia/heapQ/FindMedian.java
@@ -0,0 +1,37 @@
+package com.sophia.heapQ;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class FindMedian {
+
+//class MedianFinder {
+    private PriorityQueue<Integer> q1 = new PriorityQueue<>();
+    private PriorityQueue<Integer> q2 = new PriorityQueue<>(Collections.reverseOrder());
+
+    /** initialize your data structure here. */
+    public FindMedian() {
+    }
+
+    public void addNum(int num) {
+        q1.offer(num);
+        q2.offer(q1.poll());
+        if (q2.size() - q1.size() > 1) {
+            q1.offer(q2.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (q2.size() > q1.size()) {
+            return q2.peek();
+        }
+        return (q1.peek() + q2.peek()) * 1.0 / 2;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
