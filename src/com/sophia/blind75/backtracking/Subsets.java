3aaad22609bdc7a637589eaa6ffda106a050d5bc
yitong86 <sophiajiang86@hotmail.com>
2023-10-06 22:04:08 -0400

    addComments

--- src/com/sophia/blind75/backtracking/Subsets.java
+++ src/com/sophia/blind75/backtracking/Subsets.java
@@ -31,3 +31,30 @@ public class Subsets {
         subsets(nums, temp, index+1); // move ahead
     }
 }
+//class Solution {
+//    public List<List<Integer>> subsetsWithDup(int[] nums) {
+//        List<List<Integer>> ans = new ArrayList<>();
+//        Set<List<Integer>> res = new HashSet<>();
+//        List<Integer> ds = new ArrayList<>();
+//        Arrays.sort(nums); // Sort the input array
+//        fun(nums, 0, ds, res);
+//
+//        for (List<Integer> subset : res) {
+//            ans.add(subset);
+//        }
+//
+//        return ans;
+//    }
+//
+//    private void fun(int[] nums, int index, List<Integer> ds, Set<List<Integer>> res) {
+//        if (index == nums.length) {
+//            res.add(new ArrayList<>(ds));
+//            return;
+//        }
+//
+//        ds.add(nums[index]);
+//        fun(nums, index + 1, ds, res);
+//        ds.remove(ds.size() - 1);
+//        fun(nums, index + 1, ds, res);
+//    }
+//}
